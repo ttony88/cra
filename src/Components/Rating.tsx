d1b0784bf1e726859bd8b37df423f3a09bfc119f
ttony62 <1988anton62@gmail.com>
2024-01-10 17:50:11 +0300

    add Rating

--- src/Components/Rating.tsx
+++ src/Components/Rating.tsx
@@ -4,72 +4,17 @@ type RatingPropsType = {
 
 function Rating(props: RatingPropsType) {
 
-    if(props.value === 1) {
-        return(
-            <div>
-                <Star selected={true} />
-                <Star selected={false} />
-                <Star selected={false} />
-                <Star selected={false} />
-                <Star selected={false} />
-            </div>
-        )
-    }
-    if(props.value === 2) {
-        return(
-            <div>
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={false} />
-                <Star selected={false} />
-                <Star selected={false} />
-            </div>
-        )
-    }
-    if(props.value === 3) {
-        return(
-            <div>
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={false} />
-                <Star selected={false} />
-            </div>
-        )
-    }
-    if(props.value === 4) {
-        return(
-            <div>
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={false} />
-            </div>
-        )
-    }
-    if(props.value === 5) {
-        return(
-            <div>
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={true} />
-            </div>
-        )
-    }
     return(
-		<div>
-			<Star selected={false} />
-			<Star selected={false} />
-			<Star selected={false} />
-			<Star selected={false} />
-			<Star selected={false} />
-		</div>
-	)
+        <div>
+            <Star selected={props.value > 0} />
+            <Star selected={props.value > 1} />
+            <Star selected={props.value > 2} />
+            <Star selected={props.value > 3} />
+            <Star selected={props.value > 4} />
+        </div>
+    )
 }
-
+    
 type StarPropsType = {
     selected: boolean
 }
@@ -79,14 +24,14 @@ function Star(props: StarPropsType) {
     if(props.selected === true){
 
         return(
-            <div><b>star</b></div>
+            <span><b>star</b></span>
         )
     }
 
 	return(
-		<div>
+		<span>
 			star
-		</div>
+		</span>
 	)
 }
 
