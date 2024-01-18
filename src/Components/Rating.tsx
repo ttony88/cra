f25a1db4b617d770e2455e286bd7bb5084d13a8c
ttony62 <1988anton62@gmail.com>
2024-01-19 00:00:52 +0300

    update Reting

--- src/Components/Rating.tsx
+++ src/Components/Rating.tsx
@@ -1,35 +1,42 @@
-type RatingPropsType = {
-    value: 0 | 1 | 2 | 3 | 4 | 5
-}
+import { useState } from "react"
 
-function Rating(props: RatingPropsType) {
+function Rating() {
+
+    const [value, setValue] = useState(0)
+
+    const assignValueCreator = (value: number) => {
+        return () => setValue(value)
+    }
 
     return(
         <div>
-            <Star selected={props.value > 0} />
-            <Star selected={props.value > 1} />
-            <Star selected={props.value > 2} />
-            <Star selected={props.value > 3} />
-            <Star selected={props.value > 4} />
+            <Star assignValueCreator={assignValueCreator(1)} selected={value > 0} />
+            <Star assignValueCreator={assignValueCreator(2)} selected={value > 1} />
+            <Star assignValueCreator={assignValueCreator(3)} selected={value > 2} />
+            <Star assignValueCreator={assignValueCreator(4)} selected={value > 3} />
+            <Star assignValueCreator={assignValueCreator(5)} selected={value > 4} />
         </div>
     )
 }
     
 type StarPropsType = {
     selected: boolean
+    assignValueCreator: () => void
 }
 
 function Star(props: StarPropsType) {
 
+    const onClickHandler = () => props.assignValueCreator()
+
     if(props.selected === true){
 
         return(
-            <span><b>star</b></span>
+            <span onClick={onClickHandler}><b>star</b></span>
         )
     }
 
 	return(
-		<span>
+		<span onClick={onClickHandler}>
 			star
 		</span>
 	)
